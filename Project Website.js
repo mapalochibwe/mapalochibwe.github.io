c3e1a7142132bda4616d5da6d6b51d93c903e0b8
Squad <maverickdfh@gmail.com>
2018-03-29 17:50:27 +0200

    Submission

---
+++ Project Website.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.element', {
+strings: ["They call me: Maverick", "I'm a web developer/designer", "well...in training anyway.", "Passion is my driving fuel. ", "Languages bring nations and cultures together", "I hope that you can find beauty in the ones that built mines of wealth ", "I speak HTML5,CSS3 and a little JavaScript. ", " So contact me,tell me what best describes you", "together, we will create your dream website. "],
+typeSpeed: 60,
+backSpeed: 30,
+backDelay: 2000,
+loop: true
+});
